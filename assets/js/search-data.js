e690c0d13f203ebc0605ef2b7557bae5321cb857
jesund <20518929+jesund@users.noreply.github.com>
2025-06-26 13:19:08 +0000

    Deploying to gh-pages from @ jesund/jesund.github.io@3e97b079f1b647d0cfbd281c11d6a99671597ace 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -440,8 +440,19 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather/";
+            },},{id: "news-a-simple-inline-announcement",
+          title: 'A simple inline announcement.',
+          description: "",
+          section: "News",},{id: "news-a-long-announcement-with-details",
+          title: 'A long announcement with details',
+          description: "",
+          section: "News",handler: () => {
+              window.location.href = "/news/announcement_2/";
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
+          description: "",
+          section: "News",},{id: "news-won-the-heinz-college-konda-award-for-best-first-paper-trophy",
+          title: 'Won the Heinz College Konda Award for Best First Paper :trophy:!',
           description: "",
           section: "News",},{id: "projects-project-1",
           title: 'project 1',
